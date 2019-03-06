3e09e8ca51f3bbea818fd0070649bd4ff67306e8
visualstudio <visualstudio@122-PC13>
2019-03-06 11:40:25 -0500

    lab5 one return type is void and other is int

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,28 +21,19 @@ namespace ConsoleApp1
         }
     }
    
-       public class DelegateExcercises
-        { 
-            public delegate int  MyDelegate(int intValue);
-            public int Method1(int intMethod1)
+  public class DelegateExcercises
+    {
+        public delegate int MyDelegate();
+        void Method1()
         {
-            return intMethod1 * 2;
+            System.Console.WriteLine("myDelegate");
         }
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
-        }
-    
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            System.Console.WriteLine(result1);
-            myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            System.Console.WriteLine(result2);
 
-            }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+
         }
-         
-}
+    }
