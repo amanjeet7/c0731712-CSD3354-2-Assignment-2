1e6e98259c75d944aaaf97d069901725a066830a
visualstudio <visualstudio@122-PC13>
2019-03-06 11:08:53 -0500

    lab2:fix up bug of argument

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
+            // Student name: Amanjeet singh bajwa Student-id = c0731712
+            //CSD3354 section 2
+            // Assignment 2
+            // March 6, 2019
+
+            DelegateExcercises a = new DelegateExcercises();
+            a.Method2(); 
         }
     }
+   
+       public class DelegateExcercises
+        { 
+            public delegate void MyDelegate();
+
+            void Method1()
+            {
+                Console.WriteLine("Method1");
+                Console.ReadLine();
+            }
+            public void Method2()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                myDelegate();
+            }
+        }
+         
 }
