21cd493dcfd8bf6c8ba587f6a5ad95366c9c630b
visualstudio <visualstudio@122-PC13>
2019-03-06 12:36:30 -0500

    lab13

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,32 +8,34 @@ namespace ConsoleApp1
 {
     class program
     {
-        static void Main(string[] args)
+         public static void Main()
         {
+           
             // Student name: Amanjeet singh bajwa Student-id = c0731712
             //CSD3354 section 2
             // Assignment 2
             // March 6, 2019
 
-            DelegateExercises a = new DelegateExercises();
-            a.Method3();
+        DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
             Console.ReadLine();
         }
     }
-    public delegate void MyDelegate();
+    public delegate int MyDelegate(out int i);
     public class DelegateExercises
     {
-        void Method1()
+        int Method1(out int i)
         {
             System.Console.WriteLine("Method1");
-        }
-        public void Method3()
-        {
-
-            System.Console.WriteLine(MyDelegate.ToString());
+            i = 10;
+            return 0;
         }
     }
-
-
-}
-}
+    public void Method3()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        MyDelegate myDelegate1 = new MyDelegate(Method1);
+        MyDelegate myDelegate2 = myDelegate + myDelegate1;
+        int intValue;
+        myDelegate2(out intValue);
+    }
